f67a168441d72826c0afbf9aadda52cbfbdbacc0
Noelia <masgonoelia21@gmail.com>
2024-09-12 22:45:15 -0500

    css js áreas

--- assets/js/plantas.js
+++ assets/js/plantas.js
@@ -4,17 +4,19 @@ function getPlantas() {
       $.get(`https://pvz-2-api.vercel.app/api/plants/${planta}`, function (data) {
         const infoPlantas = `
         <div class="plant-info">
-        <h2>${data.name}</h2>
-        <p><strong>Costo:</strong> ${data["Sun cost"] || data["cost"]}</p>
-        <p><strong>Recarga:</strong> ${data.Recharge || data.recharge}</p>
-        ${data.damage ? `<p><strong>Daño:</strong>${data.damage}</p>` : ``}
-        ${data.Toughness ? `<p><strong>Dureza:</strong>${data.Toughness}</p>` : ``}
-        ${data.range ? `<p><strong>Rango:</strong>${data.range}</p>` : ``}
-        ${data.powerup ? `<p><strong>Powerup:</strong>${data.powerup}</p>` : ``}
-        ${data.Special ? `<p><strong>Special:</strong>${data.Special}</p>` : ``}
-        <p><strong>Familia:</strong> ${data.Family || data.family}</p>
-        ${data.description ? `<p><strong>Descripción:</strong>${data.description}</p>` : ``}
-        </div>`
+          <img src="https://pvz-2-api.vercel.app${data.image}" alt="${data.name}">
+          <h2>${data.name}</h2>
+          <p><strong>Costo:</strong> ${data["Sun cost"] || data["cost"]}</p>
+          ${data.Recharge || data.recharge ? `<p><strong>Recarga:</strong> ${data.Recharge || data.recharge}</p>` : ``}
+          ${data.damage ? `<p><strong>Daño:</strong> ${data.damage}</p>` : ``}
+          ${data.Toughness ? `<p><strong>Dureza:</strong> ${data.Toughness}</p>` : ``}
+          ${data.range ? `<p><strong>Rango:</strong> ${data.range}</p>` : ``}
+          ${data.powerup ? `<p><strong>Powerup:</strong> ${data.powerup}</p>` : ``}
+          ${data.Special ? `<p><strong>Especial:</strong> ${data.Special}</p>` : ``}
+          ${data.Family || data.family ? `<p><strong>Familia:</strong> ${data.Family || data.family}</p>` : ``}
+          ${data.description ? `<p class="description"><strong>Descripción:</strong> ${data.description}</p>` : ``}
+        </div>`;
+        
         $("#plant-list").append(infoPlantas);
       });
     }
--- assets/js/zombies.js
+++ assets/js/zombies.js
@@ -1,4 +1,4 @@
-function getZombies() {
+/*function getZombies() {
   $.get(`https://pvz-2-api.vercel.app/api/zombies/`, function (zombiesData) {
     for (let zombie of zombiesData) {
       $.get(`https://pvz-2-api.vercel.app/api/zombies/${zombie}`, function (data) {
@@ -16,6 +16,31 @@ function getZombies() {
   });
 }
 
+$(document).ready(function () {
+  getZombies();
+});
+*/
+function getZombies(){
+  $.get(`https://pvz-2-api.vercel.app/api/zombies`,
+      function (zombiesData){
+          for (let zombie of zombiesData){
+              $.get(`https://pvz-2-api.vercel.app/api/zombies/${zombie}`, function (data) {
+              const infoZombies = `
+              <div class="zombies-info">
+              <img src="https://pvz-2-api.vercel.app${data.image}" alt="${data.name}">
+       <h2>${data.name}</h2>
+        <p><strong>Dureza:</strong> ${data.toughness || data.toughness}</p>
+        ${data.speed ? `<p><strong>Velocidad:</strong> ${data.speed}</p>` : ``}
+        ${data.stamina ? `<p><strong>Resistencia:</strong> ${data.stamina}</p>` : ``}
+        ${data.description ? `<p class="description"><strong>Descripción:</strong> ${data.description}</p>` : ``}
+      </div>`;
+
+      $("#zombies-list").append(infoZombies);
+              });
+          }
+      }
+  );
+}
 $(document).ready(function () {
   getZombies();
 });
